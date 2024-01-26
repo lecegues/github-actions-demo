316e75d07eb58167bcbfef91f4fc9fba2abcd895
= <=>
2024-01-25 21:09:26 -0600

    Initial commit with react.js and django

---
+++ backend/djangoproject/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for djangoproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+
+Web Server Gateway Interface
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoproject.settings')
+
+application = get_wsgi_application()
